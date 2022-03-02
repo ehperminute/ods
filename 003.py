cbc6d9406c1dee028216cde1db9cd552510ca87f
ehsperminute <87844955+ehsperminute@users.noreply.github.com>
2022-03-02 11:30:05 +0500

    Update 003.py

--- 003.py
+++ 003.py
@@ -11,6 +11,11 @@ for n in range(3, num + 1, 3):
     if n % 10 != 4 and n % 10 != 7: 
         s += n 
 print(s)
+###
+s_div3 = (3 + 1_000_000_002) / 2 * 1_000_000_002 / 3
+s_div3_ends4 = (24 + 999_999_984)/ 2 * (999_999_984 + 6) / 30 
+s_div3_ends7 = (27 + 999_999_987)/ 2 *(999_999_987 + 3) / 30
+sum = s_div3 - s_div3_ends4 - s_div3_ends7
 
 '''2) На вход поступает текстовый файл из 3-х тысяч строк
 Формат файла:
@@ -22,18 +27,18 @@ print(s)
 каждое число - результат операции: 
     "результирующее целое число" = "целое число #1" применить "арифметическая операция" "целое число #2"'''
     
-    res1 = [] with open(path1) as inp:
-        for line in inp:
-            op, left, right = line.split()
-            res1.append(eval(f'{left} {op} {right}'))
-            with open(pathout, 'w') as ouf:
-                ouf.write(','.join(map(str, res1)))
+res1 = [] with open(path1) as inp:
+    for line in inp:
+        op, left, right = line.split()
+        res1.append(eval(f'{left} {op} {right}'))
+        with open(pathout, 'w') as ouf:
+            ouf.write(','.join(map(str, res1)))
                 
-    with open('input_file.txt') as inf:
-        expressions = [[line.split()[i] for i in (1, 0 , 2)] for line in inf]
-    with open('output_.txt', 'w') as ouf:
-        evaluated = (eval(' '.join(expr)) for expr in expressions)
-        ouf.write(','.join(map(str, evaluated)) + '\n')
+with open('input_file.txt') as inf:
+    expressions = [[line.split()[i] for i in (1, 0 , 2)] for line in inf]
+with open('output_.txt', 'w') as ouf:
+    evaluated = (eval(' '.join(expr)) for expr in expressions)
+    ouf.write(','.join(map(str, evaluated)) + '\n')
         
 '''3) На вход поступает два текстовый файл из 3-х тысяч строк каждый.
 
@@ -90,8 +95,13 @@ NOTiFICaTiON
 
 import json
 periodic_table = json.load(open('periodic_table.json', encoding='utf-8'))
-line = open('import_file_3.txt').read().strip()
+with open('import_file_3.txt') as inf: line = inf.read().strip()
 for key in sorted(periodic_table.keys(), key=lambda x: len(x), reverse=True):
     line = line.replace(key, periodic_table[key])
 with open('output2.txt', 'w') as ouf:
     ouf.write(line + '\n')
+###
+import re
+outlist = [periodic_table[sub] for sub in re.findall(r'[A-Z][a-z]*', line1)]
+with open('output2_.txt', 'w') as ouf:
+    print(*outlist, file=ouf, sep='')
